3eab3dadda4f4f1a9edc1b1c6220566997f9c819
David Wright <dontbothertheking@gmail.com>
2017-08-28 23:23:02 -0500

    Simplify person model

--- BasicMVVM/Model/Person.cs
+++ BasicMVVM/Model/Person.cs
@@ -8,11 +8,8 @@ namespace BasicMVVM.Model
 {
     class Person
     {
-        string _firstName;
-        string _lastName;
-
-        public string FirstName { get => _firstName; set => _firstName = value; }
-        public string LastName { get => _lastName; set => _lastName = value; }
-        public string FullName { get => $"{_firstName}  {_lastName}"; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
     }
 }
